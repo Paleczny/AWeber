ea1cdbee5d905af9bd99e676f63c3ddc38df1584
paleczny <bpaleczny00@comcast.net>
2023-12-11 13:27:34 -0600

    fix index for react 18 and add app.jsx

--- index.jsx
+++ index.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import ReactDOM from 'react-dom';
+import App from "./src/App"
 
-ReactDOM.render(
-        <>hello</>
-       ,
-    document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
