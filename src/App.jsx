8a558f116d338794fb2a32a080e84bf25cdb3211
paleczny <bpaleczny00@comcast.net>
2023-12-11 13:50:11 -0600

    add router and layout for header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Password from "./components/password/Password";
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Layout from './layouts/Layout';
+import Password from './components/password/Password';
 
 export default function App(){
     return (
-        <main>
-            <Password />
-        </main>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Password/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
