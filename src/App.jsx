99c54f55b44e7952e56c0a37d20d8a22b85ad376
paleczny <bpaleczny00@comcast.net>
2023-12-11 13:34:50 -0600

    add password component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Password from "./components/password/Password";
 
 export default function App(){
     return (
         <main>
-            app
+            <Password />
         </main>
     )
 }
